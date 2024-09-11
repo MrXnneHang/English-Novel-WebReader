46ea189d16e6aae7b71eda6f431e50a9950a7fe6
MrXnneHang <XnneHang@gmail.com>
2024-09-11 14:52:24 +0800

    refactor：上传文件的部分

---
+++ view/src/components/EbookHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="file-upload">
+    <input type="file" @change="onFileChange" accept=".txt" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    onFileChange: Function, // 从父组件接收上传文件时触发的方法
+  },
+};
+</script>
+
+<style scoped>
+.file-upload {
+  padding: 20px;
+  background-color: #f8f8f8;
+  text-align: center;
+}
+input[type="file"] {
+  padding: 10px;
+  font-size: 16px;
+}
+</style>
