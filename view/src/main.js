7f09040d2345c94d4bc118643571987aceb12f9d
MrXnneHang <XnneHang@gmail.com>
2024-09-11 14:57:21 +0800

    readme:完成task2,task3

--- view/src/main.js
+++ view/src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router/index.js';
-import vuetify from './plugins/vuetify'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
